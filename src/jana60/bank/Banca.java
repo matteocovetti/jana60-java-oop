a98f06e7f76c383dbd39d4f42664f8de0ae507d3
matteocovetti <covetti.matteo@gmail.com>
2022-06-20 11:23:55 +0200

    add bank - live coding

--- src/jana60/bank/Banca.java
+++ src/jana60/bank/Banca.java
@@ -15,8 +15,49 @@ public class Banca {
 		int numeroConto = random.nextInt(1000) + 1;
 
 		Conto conto = new Conto(numeroConto, nome);
+
 		System.out.print("Grazie abbiamo creato il conto numero " + conto.getNumeroConto());
 		System.out.println(" intestato a " + conto.getProprietario());
+
+		boolean finito = false;
+
+		while (!finito) {
+			// menu a scelta multipla
+			System.out.println("Cosa vuoi fare?");
+			System.out.println("1 per prelevare - 2 per versare - 3 per uscire");
+			String scelta = scan.nextLine();
+			// in base alla scelta utente
+			switch (scelta) {
+			case "1":
+				// prelevare
+				System.out.println("Quanto vuoi prelevare?");
+				double sommaDaPrelevare = Double.parseDouble(scan.nextLine());
+				boolean risultatoPrelievo = conto.prelievo(sommaDaPrelevare);
+				if (risultatoPrelievo) {
+					System.out.println("Prelievo avvenuto con successo");
+				} else {
+					System.out.println("Impossibile prelevare. Il saldo non è sufficente");
+				}
+				System.out.println("Il tuo saldo corrente è " + conto.getSaldoFormattato());
+				break;
+			case "2":
+				// versare
+				System.out.println("Quanto vuoi versare?");
+				double sommaDaVersare = Double.parseDouble(scan.nextLine());
+				conto.deposito(sommaDaVersare);
+				System.out.println("Dopo il versamento il tuo saldo è " + conto.getSaldoFormattato());
+				break;
+			case "3":
+				// uscire
+				System.out.println("Grazie per aver usato Java Bank");
+				finito = true;
+				break;
+			default:
+				// gestire input sbagliato
+				System.out.println("Scelta non valida.");
+				break;
+			}
+		}
 
 		scan.close();
 	}
